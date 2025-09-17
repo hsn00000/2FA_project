bf53cb34e2450aae7f8e6d9959d2aa62e45050b7
hsn00000 <kanicihasan90@gmail.com>
2025-09-17 11:58:31 +0200

    FEAT: 2FA

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,7 +8,9 @@ use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticator
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -35,39 +37,40 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
     #[Route(path: '/enable2fa', name: 'app_enable_2fa')]
-    public function enable2fa(GoogleAuthenticatorInterface $googleAuthenticator, EntityManagerInterface $entityManager, Request $request): Response
+    #[IsGranted('ROLE_USER')] // Assurez-vous que l'utilisateur est connecté
+    public function enable2fa(GoogleAuthenticatorInterface $googleAuthenticator, EntityManagerInterface $entityManager, Request $request, SessionInterface $session): Response
     {
+        // Récupère l'utilisateur actuellement connecté
         $user = $this->getUser();
-        $secret = $googleAuthenticator->generateSecret();
 
-
+        $secret = $session->get('2fa_secret');
+        if (!$secret) {
+            // Génère un nouveau secret et le stocke dans la session
+            $secret = $googleAuthenticator->generateSecret();
+            $session->set('2fa_secret', $secret);
+        }
+        // Assigne le secret à l'utilisateur
+        $user->setGoogleAuthenticatorSecret($secret);
+        // Crée et gère le formulaire
         $myForm = $this->createForm(Enable2faType::class);
         $myForm->handleRequest($request);
 
         if ($myForm->isSubmitted() && $myForm->isValid()) {
             $data = $myForm->getData();
-            $user = $this->getUser();
 
             // Vérifie le code saisi par l'utilisateur
             if ($googleAuthenticator->checkCode($user, $data['secret'])) {
                 $this->addFlash('success', 'L\'authentification à deux facteurs a été activée avec succès.');
-                $user->setGoogleAuthenticatorSecret($secret);
                 $entityManager->persist($user);
                 $entityManager->flush();
-                return $this->redirectToRoute('app_logout');
+                return $this->redirectToRoute('app_login');
             } else {
                 $this->addFlash('error', 'Le code de vérification est invalide. Veuillez réessayer.');
             }
         }
 
-
-
-
         //Génère le QR code
         $qrCodeContent = $googleAuthenticator->getQRContent($user);
-
-
-
 
         return $this->render('enable2fa.html.twig', [
             'secret' => $secret,
